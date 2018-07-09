5074b6e202d6e04aec13df69a4a963e6c873843f
Caitlin Sedwick <cait@cait-albook-5.local>
2018-07-09 13:38:32 -0700

    Add New Item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["find Mike", "buy Eggos", "destroy Demogorgon"]
+    var itemArray = ["find Mike", "buy Eggos", "destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,7 +54,35 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    // MARK - Add new items
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //function variable to hold alert alertTextField data
+        var textField = UITextField()
+        //create an alert "Add New Todoey Item"...
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        //...that has an action "Add Item"
+        let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
+            //declare what will happen once the user clicks the add item button on our UIAlert
+            
+            //append item from alertTextField (below) to itemArray
+            self.itemArray.append(textField.text ?? "New Item")
+            
+            //reload the Table View to display the new item
+            self.tableView.reloadData()
+        }
+        //display the action with text field
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            //extend the scope of alertTextField to the entire function so we can retrieve the contents of the alert text field
+            textField = alertTextField
+            
+        }
+        
+        present(alert, animated: true, completion: nil)
+        }
+    
 
 }
 
