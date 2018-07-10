5f7d7606bd4a681a1a8e761d3b343f177344887d
Caitlin Sedwick <cait@cait-albook-5.local>
2018-07-10 08:14:40 -0700

    convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["find Mike", "buy Eggos", "destroy Demogorgon"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+       
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "slay Demogorgon"
+        itemArray.append(newItem3)
+        
+        //save items to the UserDefaults singleton.  This is bad.  Don't do it even if it looks like it works.
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,24 +46,30 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //fix problem of checkmark appearing on recycled rows (part 2 of fix)
+        //replace if/else for cell.accessoryType when item.done == true using a...
+        //Ternary operator ==>
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
 // MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //diagnostic
-        //print(itemArray[indexPath.row])
         
-        //add a checkmark when a cell is selected, or remove checkmark if cell was already selected
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //fix the problem of checkmark appearing on recycled rows (part 1 of fix)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+    
+        //reload the table view for each item to refresh checkmarks
+        tableView.reloadData()
         
         //make the row unhighlight itself after it's been clicked
         tableView.deselectRow(at: indexPath, animated: true)
@@ -71,7 +89,9 @@ class TodoListViewController: UITableViewController {
             //declare what will happen once the user clicks the add item button on our UIAlert
             
             //append item from alertTextField (below) to itemArray
-            self.itemArray.append(textField.text ?? "New Item")
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             //add to defaults plist file so data can persist after app is e.g. force-quit
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Caitlin Sedwick on 7/9/18.
+//  Copyright © 2018 Caitlin Sedwick. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
